c0ea81e777dcc3d37f079e4ef8c45ee8ba54193e
mariostoeffler <52000392@edu.campus02.at>
2024-01-22 19:02:15 +0100

    added Tiefensuche 06

--- src/A06_Tiefensuche/Tiefensuche.java
+++ src/A06_Tiefensuche/Tiefensuche.java
@@ -1,40 +1,65 @@
 package A06_Tiefensuche;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import A05_Breitensuche.BaseTree;
 import A05_Breitensuche.Node;
 
 public class Tiefensuche extends BaseTree<Film> {
 
-	@Override
-	/**
-	 * Sortierkriterium im Baum: Länge des Films
-	 */
-	protected int compare(Film a, Film b) {
+    @Override
+    /**
+     * Sortierkriterium im Baum: Länge des Films
+     */
+    protected int compare(Film a, Film b) {
 
-		return 0;
-	}
+        return Double.compare(a.getLänge(), b.getLänge());
+    }
 
-	/**
-	 * Retourniert die Titelliste der Film-Knoten des Teilbaums in symmetrischer Folge (engl. in-order, d.h. links-Knoten-rechts)
-	 * @param node Wurzelknoten des Teilbaums
-	 * @return Liste der Titel in symmetrischer Reihenfolge
-	 */
-	public List<String> getNodesInOrder(Node<Film> node) {
+    /**
+     * Retourniert die Titelliste der Film-Knoten des Teilbaums in symmetrischer Folge (engl. in-order, d.h. links-Knoten-rechts)
+     *
+     * @param node Wurzelknoten des Teilbaums
+     * @return Liste der Titel in symmetrischer Reihenfolge
+     */
+    public List<String> getNodesInOrder(Node<Film> node) {
 
-		return null;
-	}
-	
-	/**
-	 * Retourniert Titelliste jener Filme, deren Länge zwischen min und max liegt, in Hauptreihenfolge (engl. pre-order, d.h. Knoten-links-rechts)
-	 * @param min Minimale Länge des Spielfilms
-	 * @param max Maximale Länge des Spielfilms
-	 * @return Liste der Filmtitel in Hauptreihenfolge
-	 */
-	public List<String> getMinMaxPreOrder(double min, double max) {
+        List<String> result = new ArrayList<>();
+        Stack<Node<Film>> stack = new Stack<>();
 
-		return null;
-	}
+        Node<Film> current = node;
+
+        while (stack.isEmpty() == false || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.getLeft();
+            }
+
+            if (stack.isEmpty() == false) {
+                current = stack.pop();
+                result.add(current.getValue().getTitel());
+                current = current.getRight();
+            }
+
+        }
+        return result;
+
+    }
+
+
+
+    /**
+     * Retourniert Titelliste jener Filme, deren Länge zwischen min und max liegt, in Hauptreihenfolge (engl. pre-order, d.h. Knoten-links-rechts)
+     *
+     * @param min Minimale Länge des Spielfilms
+     * @param max Maximale Länge des Spielfilms
+     * @return Liste der Filmtitel in Hauptreihenfolge
+     */
+    public List<String> getMinMaxPreOrder(double min, double max) {
+
+        return null;
+    }
 
 }
