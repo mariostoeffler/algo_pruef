2150f380935bcd119e0ad2da5ef828f2132b4d95
mariostoeffler <52000392@edu.campus02.at>
2024-01-24 19:47:11 +0100

    added dijkstra

--- src/A10_DijkstraPQShortestPath/DijkstraPQShortestPath.java
+++ src/A10_DijkstraPQShortestPath/DijkstraPQShortestPath.java
@@ -1,37 +1,67 @@
 package A10_DijkstraPQShortestPath;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DijkstraPQShortestPath extends FindWay {
-	private int[] dist;
+    private int[] dist;
 
-	public DijkstraPQShortestPath(Graph graph) {
-		super(graph);
-	}
+    public DijkstraPQShortestPath(Graph graph) {
+        super(graph);
+    }
 
-	/**
-	 * Startentfernung initialisieren
-	 * 
-	 * @param from
-	 *            Startknoten
-	 */
-	protected void initPathSearch() {
-		int numv = graph.numVertices();
-		dist = new int[numv];
-		for (int i = 0; i < numv; i++) {
-			dist[i] = 9999; // Summen im Graph dürfen nie mehr ergeben
-		}
-	}
+    /**
+     * Startentfernung initialisieren
+     *
+     * @param
+     */
+    protected void initPathSearch() {
+        int numv = graph.numVertices();
+        dist = new int[numv];
+        for (int i = 0; i < numv; i++) {
+            dist[i] = Integer.MAX_VALUE; // Summen im Graph dürfen nie mehr ergeben
+            pred[i] = -1;
+        }
+    }
 
-	/**
-	 * Berechnet *alle* kürzesten Wege ausgehend vom Startknoten Setzt dist[]-
-	 * und pred[]-Arrays, kein Rückgabewert
-	 * 
-	 * @param from
-	 *            Startknoten
-	 */
-	protected boolean calculatePath(int from, int to) {
+    /**
+     * Berechnet *alle* kürzesten Wege ausgehend vom Startknoten Setzt dist[]-
+     * und pred[]-Arrays, kein Rückgabewert
+     *
+     * @param from Startknoten
+     */
+    protected boolean calculatePath(int from, int to) {
 
-		// TODO: IHRE IMPLEMENTIERUNG
-		return true;
-	}
+        dist[from] = 0;
+        VertexHeap heap = new VertexHeap(graph.numVertices());
+        for (int i = 0; i < graph.numVertices(); i++) {
+            heap.insert(new Vertex(i, dist[i]));
+        }
+
+
+
+
+        while (!heap.isEmpty()) {
+            int v = heap.remove().vertex;
+            List<WeightedEdge> edges = graph.getEdges(v);
+            for (WeightedEdge we : edges) {
+                int newCost = dist[v] + we.weight;
+                int to_vertex = we.to_vertex;
+
+                if (newCost < dist[to_vertex]) {
+                    dist[to_vertex] = newCost;
+                    heap.setCost(to_vertex, newCost);
+                    pred[to_vertex] = v;
+                }
+
+
+            }
+
+        }
+
+        return (dist[to] != Integer.MAX_VALUE);
+
+
+    }
 }
--- src/A10_DijkstraPQShortestPath/FindWay.java
+++ src/A10_DijkstraPQShortestPath/FindWay.java
@@ -46,7 +46,12 @@ public abstract class FindWay {
 	protected ArrayList<Integer> createWay(int from, int to) {
 		ArrayList<Integer> way = new ArrayList<Integer>();
 
-		// TODO: IHRE IMPLEMENTIERUNG
+		int i = to;
+		while (i != from) {
+			way.add(0, i);
+			i = pred[i];
+		}
+		way.add(0, from);
 
 		return way;
 	}
